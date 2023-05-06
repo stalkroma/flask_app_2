7fe555e5c816253b848abf185f2979455f2a928f
stalkroma <roman.podolski1997@gmail.com>
2023-05-06 23:07:26 +0200

    dodałem nowe coś jeszcze

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
